206b3715b75f6a2f21c9c622cf8e752d4204c613
Paul Jones <shirlypunk@hotmail.com>
2018-05-19 23:03:51 -0400

    removed comments

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -12,14 +12,6 @@ namespace Crunchy.Models {
 
         public DbSet<StatusSet> StatusSets { get; set; }
  
-        //public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
-        //}
-
-        /*public TodoContext() {
-            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
-            OnConfiguring(optionsBuilder);
-        }*/
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlite("Data Source=crunchy.db");    
         }
