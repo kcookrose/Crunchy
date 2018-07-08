5622bc95dcf6d2af44d0a4b3df33d46db4bf9c54
Paul Jones <shirlypunk@hotmail.com>
2018-07-07 20:56:33 -0400

    removed usage of static files

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,6 @@ namespace Crunchy
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            app.UseStaticFiles();
             app.UseMvc();
         }
     }
